fc3a56d1ba016b00461f44664de3badd38dedcbe
1999praveena <praveenaarokiaraj@gmail.com>
2017-11-25 19:11:45 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=1,i;t;
+printf("enter value");
+scanf("%d",&t);
+for(i=1;i<=t;i++)
+a=a*i;
+printf("factorial is %d",a);
+getch();
+return 0;
+}
